bf9ffc47f978b7c9b192fdc854ec8d13b6283837
Alex (Alexander) Nilsson <alexander.nilsson4@learnet.se>
2024-04-23 16:07:13 +0200

    Movment

--- Assets/Scripts/PlayerMovment.cs
+++ Assets/Scripts/PlayerMovment.cs
@@ -1,26 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.InputSystem.Users;
 
 public class PlayerMovment : MonoBehaviour
 {
+    Rigidbody rb;
+
     Vector2 movementInput;
+
+    float XDirection = 1;
+    float ZDirection = 1;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        XDirection = movementInput.x;
+        ZDirection = movementInput.y;
+
+        rb.velocity = new Vector3 (XDirection, 0, ZDirection);
     }
 
     void OnMove(InputValue value)
     {
-        movementInput = value.Get<Vector2>();
+        movementInput = value.Get<Vector3>();
     }
 }
