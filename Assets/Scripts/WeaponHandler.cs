deeb63cb4417428146b4c6e42486b9b875a3366a
Alex (Alexander) Nilsson <alexander.nilsson4@learnet.se>
2024-04-30 16:09:45 +0200

    Start Of Weapon

--- Assets/Scripts/WeaponHandler.cs
+++ Assets/Scripts/WeaponHandler.cs
@@ -2,6 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum WeaponState
+{
+    unarmed = 0,
+    HitScan = 1,
+    Projectile = 2,
+    Melee = 3,
+    Total
+}
+
 public class WeaponHandler : MonoBehaviour
 {
     #region Melee
