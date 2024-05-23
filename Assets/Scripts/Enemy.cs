053136d3b5e90dda8fa47b3838b8cb6bf6964a78
Alex (Alexander) Nilsson <alexander.nilsson4@learnet.se>
2024-05-23 13:32:53 +0200

    Minor Stuff

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+
+    float health = 5f;
+    float explosionKnockbackStrenght = 2;
+
+    Rigidbody rbEnemy;
+
+    bool knockBack;
+
+    #region Gravity
+
+    public bool isGrounded;
+
+    public Transform groundCheck;
+
+    public float groundDistance = 0.4f;
+    public float gravity = -9.82f;
+
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    #endregion
+
+    private void Start()
+    {
+        rbEnemy = GetComponent<Rigidbody>();
+
+        knockBack = false;
+    }
+
+    void Update()
+    {
+        if(health <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1.0f;
+        }
+        if (!knockBack)
+        {
+            velocity.y += Mathf.Sqrt(explosionKnockbackStrenght * -2 * gravity);
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+
+        if(!knockBack)
+        {
+            rbEnemy.velocity = velocity;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 7)
+        {
+            knockBack = true;
+
+            rbEnemy.velocity = velocity;
+
+            knockBack = false; 
+        }
+    }
+}
---
+++ Assets/Scripts/Entitys.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entitys : MonoBehaviour
+{
+    #region Gravity
+
+    public bool isGrounded;
+
+    public Transform groundCheck;
+
+    public float groundDistance = 0.4f;
+    public float gravity = -9.82f;
+
+    public LayerMask groundMask;
+
+    Vector3 velocity;
+
+    #endregion
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1.0f;
+        }
+
+        velocity.y += gravity * Time.deltaTime;
+    }
+}
